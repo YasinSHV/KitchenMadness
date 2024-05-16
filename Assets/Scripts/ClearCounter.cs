db02c120a9f04381a87e1894ac8ad85e136cb908
YasinSHV <yshabaniv@gmail.com>
2024-05-16 13:37:25 +0330

    Added Interaction Foundation, Cleaned playerUpdate

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCounter : MonoBehaviour
+{
+    public void Interact() 
+    {
+        //intract with counter
+        Debug.Log("Intract Called");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,16 +8,30 @@ public class Player : MonoBehaviour
     //For Fields we start with camelCase
     [SerializeField] private float speed = 1f, rotationSpeed = 10f;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private LayerMask countersLayer;
 
     private bool isWalking;
+    private Vector3 lastMovementDirection;
 
     private void Update()
+    {
+        HandleMovement();
+        HandleIntractions();
+    }
+
+    //For functions we have PascalCase
+    public bool IsWalking() 
+    {
+        return isWalking;
+    }
+
+    private void HandleMovement() 
     {
         //Moved vector logic to the InputManager Class
         Vector2 inputVector = inputManager.GetMovementVectorNormalized();
 
         //convert vector2 to vector3
-        Vector3 movementDirection = new Vector3(inputVector.x, 0,inputVector.y);
+        Vector3 movementDirection = new Vector3(inputVector.x, 0, inputVector.y);
 
         //Collision handling
         float moveDistance = speed * Time.deltaTime;
@@ -26,7 +40,7 @@ public class Player : MonoBehaviour
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movementDirection, moveDistance);
 
         //Handling diagonal collison detection
-        if(!canMove) 
+        if (!canMove)
         {
             //Try moving on X
             Vector3 moveOnX = new Vector3(movementDirection.x, 0, 0).normalized;
@@ -35,7 +49,7 @@ public class Player : MonoBehaviour
             {
                 movementDirection = moveOnX;
             }
-            else 
+            else
             {
                 //Try moving on z
                 Vector3 moveOnZ = new Vector3(0, 0, movementDirection.z).normalized;
@@ -59,9 +73,24 @@ public class Player : MonoBehaviour
         transform.forward = Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotationSpeed);
     }
 
-    //For functions we have PascalCase
-    public bool IsWalking() 
+    private void HandleIntractions()
     {
-        return isWalking;
+        Vector2 inputVector = inputManager.GetMovementVectorNormalized();
+        Vector3 movementDirection = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (movementDirection != Vector3.zero) 
+        {
+            lastMovementDirection = movementDirection;
+        }
+
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, lastMovementDirection, out RaycastHit raycastHit, interactDistance)) ;
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Is clearCounter
+                clearCounter.Interact();
+            }
+        }
     }
 }
