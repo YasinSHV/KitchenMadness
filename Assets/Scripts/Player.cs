0d46342bf23431883a27542c6dd8e51ea5ba8ede
YasinSHV <yshabaniv@gmail.com>
2024-05-16 12:41:02 +0330

    Added through script collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,8 +16,42 @@ public class Player : MonoBehaviour
         //Moved vector logic to the InputManager Class
         Vector2 inputVector = inputManager.GetMovementVectorNormalized();
 
+        //convert vector2 to vector3
         Vector3 movementDirection = new Vector3(inputVector.x, 0,inputVector.y);
-        transform.position += movementDirection * speed * Time.deltaTime;
+
+        //Collision handling
+        float moveDistance = speed * Time.deltaTime;
+        float playerHeight = 2f;
+        float playerRadius = 0.7f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movementDirection, moveDistance);
+
+        //Handling diagonal collison detection
+        if(!canMove) 
+        {
+            //Try moving on X
+            Vector3 moveOnX = new Vector3(movementDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveOnX, moveDistance);
+            if (canMove)
+            {
+                movementDirection = moveOnX;
+            }
+            else 
+            {
+                //Try moving on z
+                Vector3 moveOnZ = new Vector3(0, 0, movementDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveOnZ, moveDistance);
+                if (canMove)
+                {
+                    movementDirection = moveOnZ;
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += movementDirection * speed * Time.deltaTime;
+        }
+
 
         //if the move direction isnt zero the player is walking
         isWalking = movementDirection != Vector3.zero;
